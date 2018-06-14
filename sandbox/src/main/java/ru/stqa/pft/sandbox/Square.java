580e0a48cd1fc7c2ebbf94e2cf4bd06d4268573a
sergtimosh <sergtimosh@gmail.com>
2018-06-15 01:53:00 +0300

    Rectangle and Square classes are edited

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -1,4 +1,15 @@
 package ru.stqa.pft.sandbox;
 
 public class Square {
+
+  public double l;
+
+  public Square(double l) {
+
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l  * this.l;
+  }
 }
