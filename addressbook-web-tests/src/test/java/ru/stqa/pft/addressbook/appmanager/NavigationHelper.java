297e89e773646bb1da88bf527cb81228eaba8107
sergtimosh <sergtimosh@gmail.com>
2018-06-23 17:00:34 +0300

    Browsers selection added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -1,16 +1,16 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase {
 
-  public NavigationHelper(FirefoxDriver wd) {
+  public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
   public void goToGroupPage() {
-    click(By.linkText("groups"));
+    click(By.xpath("groups"));
   }
 
   public void goToHomePage() {
