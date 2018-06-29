6bcfb60f9f5897ce2a0b099531f4082169d368e2
sergtimosh <sergtimosh@gmail.com>
2018-06-29 23:36:09 +0300

    before/after List comparison added to GroupDeletionTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -8,18 +8,21 @@ import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
-  @Test
-  public void testGroupDeletion() {
-    app.getNavigationHelper().goToGroupPage();
-    if (! app.getGroupHelper().isThereAGroup()) {
-      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-    }
-    List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(before.size() - 1);
-    app.getGroupHelper().deleteSelectedGroups();
-    app.getGroupHelper().returnToGroupPage();
-    List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before.size() - 1);
-  }
+   @Test
+   public void testGroupDeletion() {
+      app.getNavigationHelper().goToGroupPage();
+      if (!app.getGroupHelper().isThereAGroup()) {
+         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+      }
+      List<GroupData> before = app.getGroupHelper().getGroupList();
+      app.getGroupHelper().selectGroup(before.size() - 1);
+      app.getGroupHelper().deleteSelectedGroups();
+      app.getGroupHelper().returnToGroupPage();
+      List<GroupData> after = app.getGroupHelper().getGroupList();
+      Assert.assertEquals(after.size(), before.size() - 1);
 
+      before.remove(before.size() - 1);
+      Assert.assertEquals(before, after);
+   }
 }
+
