3d7df48a4144afa1aeb365ff6054c7db06075a48
sergtimosh <sergtimosh@gmail.com>
2018-06-30 23:35:47 +0300

    Group creation tests now can compare unordered lists

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,12 +14,19 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test2", null, null);
+    GroupData group = new GroupData("test1", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    group.setId(after.stream().max((GroupData o1, GroupData o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    int max = 0;
+    for (GroupData g: after) {
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+
+    group.setId(max);
     before.add(group);
     Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
