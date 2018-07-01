d7b48c0a6635324f47ba8de7d0157267bfbdb699
sergtimosh <sergtimosh@gmail.com>
2018-07-01 22:03:13 +0300

    Contact deletion test selects and delets contact now by index, and it gives more flexibility to chose any contact from the retrived "before" list

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -15,7 +15,7 @@ public class ContactDeletionTests extends TestBase {
       if (!app.getContactHelper().isThereAContact()) {
          app.getContactHelper().createContact(new ContactData("Serhiy", "Tymoshenko", "17. Sichovy str.\n116 apt", "serdtimosh@gmail.com", "+0472313950", "+380945643839", "test1"), true);
       }
-      app.getContactHelper().selectContact();
+      app.getContactHelper().selectContact(before.size() - 1);
       app.getContactHelper().deleteSelectedContact();
       app.getContactHelper().acceptContactDeletion();
       app.getNavigationHelper().goToHomePage();
