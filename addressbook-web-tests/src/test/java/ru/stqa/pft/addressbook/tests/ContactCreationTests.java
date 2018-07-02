2ffd10688a845846f1607e6e31adb8a28fb5753e
sergtimosh <sergtimosh@gmail.com>
2018-07-02 15:00:22 +0300

    Loop, to determine maximum ID value replaced with lambda expression in Contact creation tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.HashSet;
 import java.util.List;
@@ -14,18 +13,13 @@ public class ContactCreationTests extends TestBase {
    public void testContactCreation() {
       app.getNavigationHelper().goToHomePage();
       List<ContactData> before = app.getContactHelper().getContactList();
-      ContactData group = new ContactData("Serhiy", "Tymoshenko", "17. Sichovy str.\n116 apt", "serdtimosh@gmail.com", "+0472313950", "+380945643839", "test1");
+      ContactData group = new ContactData("Arsen", "Tymoshenko", "17. Sichovy str.\n116 apt", "serdtimosh@gmail.com", "+0472313950", "+380945643839", "test1");
       app.getContactHelper().createContact(group, true);
       List<ContactData> after = app.getContactHelper().getContactList();
       Assert.assertEquals(after.size(), before.size() + 1);
 
-      int max = 0;
-      for (ContactData g : after) {
-         if (g.getId() > max) {
-            max = g.getId();
-         }
-      }
-      group.setId(max);
+
+      group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
       before.add(group);
       Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
    }
