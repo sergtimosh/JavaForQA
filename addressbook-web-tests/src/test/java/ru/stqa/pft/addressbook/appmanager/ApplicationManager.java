9103ff58d0306980c31c02eafaae06c04a881fe6
sergtimosh <sergtimosh@gmail.com>
2018-08-15 00:20:01 +0300

    Win7 used for platform

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -53,7 +53,7 @@ public class ApplicationManager {
       } else {
          DesiredCapabilities capabilities = new DesiredCapabilities();
          capabilities.setBrowserName(browser);
-         capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "sierra")));
+         capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
          wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
       }
       wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
